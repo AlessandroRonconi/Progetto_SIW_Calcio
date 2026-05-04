f60c07e6f6a1cd1ca0b4e3b27fbb18dacb3eaa99
Ale Ronconi <aleronconi@diadia.com>
2026-05-04 17:23:03 +0200

    new file:   football/src/main/java/it/uniroma3/siw/football/controller/GlobalExceptionHandler.java

---
+++ football/src/main/java/it/uniroma3/siw/football/controller/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package it.uniroma3.siw.football.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import it.uniroma3.siw.football.exception.GameNotFoundException;
+import it.uniroma3.siw.football.exception.TournamentNotFoundException;
+import it.uniroma3.siw.football.exception.UserNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TournamentNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleTournamentNotFound(TournamentNotFoundException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/404";
+    }
+
+    @ExceptionHandler(TournamentNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleGameNotFound(GameNotFoundException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/404";
+    }
+
+    @ExceptionHandler(TournamentNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleUserNotFound(UserNotFoundException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/404";
+    }
+}
