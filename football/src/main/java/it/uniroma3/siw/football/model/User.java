8706ba3a0110138c4b95e798dbc835c3ebf21871
AlessandroRonconi <ronconi.alessandro46@gmail.com>
2026-05-07 22:21:39 +0200

    modified:   football/src/main/java/it/uniroma3/siw/football/model/User.java 	modified:   football/src/main/resources/import.sql

--- football/src/main/java/it/uniroma3/siw/football/model/User.java
+++ football/src/main/java/it/uniroma3/siw/football/model/User.java
@@ -11,9 +11,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
