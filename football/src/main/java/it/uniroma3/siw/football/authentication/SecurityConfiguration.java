3808b3a8a6d480f797c5b014286634651295b0f8
AlessandroRonconi <ronconi.alessandro46@gmail.com>
2026-05-22 21:18:26 +0200

    modified:   football/src/main/java/it/uniroma3/siw/football/authentication/SecurityConfiguration.java

--- football/src/main/java/it/uniroma3/siw/football/authentication/SecurityConfiguration.java
+++ football/src/main/java/it/uniroma3/siw/football/authentication/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration {
     protected SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeHttpRequests(authorize -> {
-            authorize.requestMatchers(HttpMethod.GET, "/", "/index", "/register", "/css/**", "/tournaments/list", "/tournaments/tournament", "/teams/list", "/teams/team").permitAll();
+            authorize.requestMatchers(HttpMethod.GET, "/", "/index", "/register", "/css/**", "/tournaments", "/tournaments/list", "/tournaments/tournament", "/teams", "/teams/list", "/teams/team").permitAll();
             authorize.requestMatchers(HttpMethod.POST, "/register", "/login").permitAll();
             authorize.requestMatchers(HttpMethod.GET, "/admin/**").hasAnyAuthority("ADMIN");
             authorize.requestMatchers(HttpMethod.POST, "/admin/**").hasAnyAuthority("ADMIN");
